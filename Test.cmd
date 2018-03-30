4297ed47a613f0cec507972bb595e281c611b42a
Anton Firszov <antonfir@gmail.com>
2018-03-30 19:31:14 +0200

    travissssss????

--- Test.cmd
+++ Test.cmd
@@ -1,6 +1,6 @@
 cd .\AppVeyorDotnetSandbox
 dotnet xunit -framework netcoreapp1.1
 dotnet xunit -framework netcoreapp2.0 --fx-version 2.0.0
-dotnet xunit -framework net461
-dotnet xunit -framework net461 -x86
+dotnet xunit -framework net462
+dotnet xunit -framework net462 -x86
 rem mono.exe .\xunit.runner.console\2.3.1\tools\net452\xunit.console.exe .\bin\Release\net461\AppVeyorDotnetSandbox.dll
--- Test.sh
+++ Test.sh
@@ -1,3 +1,4 @@
 cd ./AppVeyorDotnetSandbox
-dotnet xunit
+dotnet xunit -framework netcoreapp2.0
+dotnet xunit -framework netcoreapp1.1
 #mono ~/.nuget/packages/xunit.runner.console/2.3.1/tools/net452/xunit.console.exe ./bin/Release/net461/AppVeyorDotnetSandbox.dll
