f875fd04d832d045cc630997ede8f1761edf0008
Anton Firszov <antonfir@gmail.com>
2018-03-30 18:23:28 +0200

    or maybe now?

---
+++ AppVeyorDotnetSandbox/Test.cmd
@@ -0,0 +1,5 @@
+dotnet xunit -framework netcoreapp1.1
+dotnet xunit -framework netcoreapp2.0 --fx-version 2.0.0
+dotnet xunit -framework net461
+dotnet xunit -framework net461 -x86
+mono.exe ..\packages\xunit.runner.console\2.3.1\tools\net452\xunit.console.exe .\bin\Release\net461\AppVeyorDotnetSandbox.dll
