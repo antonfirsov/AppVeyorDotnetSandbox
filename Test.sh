a0e263fd91b6f3d696d6ea52cd37c80baa0e70b3
Anton Firszov <antonfir@gmail.com>
2018-03-30 20:03:32 +0200

    soros!

--- Test.sh
+++ Test.sh
@@ -4,8 +4,8 @@ DOTNETVER_FULL=$(dotnet --version| cut -c 1-5)
 DOTNETVER_SHORT=$(dotnet --version| cut -c 1-3)
 
 echo "dotnet version full: " $DOTNETVER_FULL
-echo "dotnet version short: " $DOTNETVER_FULL
+echo "dotnet version short: " $DOTNETVER_SHORT
 
 cd ./AppVeyorDotnetSandbox
-dotnet xunit -framework netcoreapp$DOTNETVER_SHORT --fx-version $DOTNETVER_FULL
+dotnet xunit -framework netcoreapp$DOTNETVER_SHORT
 #mono ~/.nuget/packages/xunit.runner.console/2.3.1/tools/net452/xunit.console.exe ./bin/Release/net461/AppVeyorDotnetSandbox.dll
