11b64f11562b5881583f16b457db66c6f08b14a5
Anton Firszov <antonfir@gmail.com>
2018-03-30 23:32:41 +0200

    + mono test execution

--- Test.cmd
+++ Test.cmd
@@ -1,9 +1,15 @@
 cd .\AppVeyorDotnetSandbox
 
-dotnet restore
+dotnet build
+dotnet xunit -nobuild -framework netcoreapp1.1
+dotnet xunit -nobuild -framework netcoreapp2.0 --fx-version 2.0.0
+dotnet xunit -nobuild -framework net462
+dotnet xunit -nobuild -framework net462 -x86
 
-dotnet xunit -framework netcoreapp1.1
-dotnet xunit -framework netcoreapp2.0 --fx-version 2.0.0
-dotnet xunit -framework net462
-dotnet xunit -framework net462 -x86
-rem mono.exe .\xunit.runner.console\2.3.1\tools\net452\xunit.console.exe .\bin\Release\net461\AppVeyorDotnetSandbox.dll
+@echo off
+set TEST_DLL_PATH="%~dp0%~1AppVeyorDotnetSandbox\bin\Release\net461\AppVeyorDotnetSandbox.dll"
+cd %HOMEPATH%\.nuget\packages\xunit.runner.console\2.3.1\tools\net452\
+@echo on
+mono .\xunit.console.exe %TEST_DLL_PATH%
+
+cd %~dp0%~1
